0538501287c06531ecd842200d0d5867351a1b4f
frankkib <frankkibagendi13@gmail.com>
2022-12-13 08:46:59 +0000

    simple shell program

---
+++ simple.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the beginning of the fuction
+ * @ac: the agrument counter
+ * @av: the argument vector
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int num = 0;
+	char *temp = "hello kenya";
+
+	 printf("%s\n", *av);
+	 printf("%d\n", ac);
+	if (!temp[num])
+	{
+		printf("%d\n", temp[num]);
+		temp++;
+	}
+	return (0);
+}
